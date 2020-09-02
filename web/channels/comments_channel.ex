21cfb7a931860c495fb4188929f899b79de9d657
isasiluispy <isasiluispy@gmail.com>
2020-09-02 13:49:00 -0400

    comments can be shown with their respective users

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -6,7 +6,7 @@ defmodule Discuss.CommentsChannel do
     topic_id = String.to_integer topic_id
     topic = Topic
       |> Repo.get(topic_id)
-      |> Repo.preload(:comments)
+      |> Repo.preload(comments: [:user])
 
 
     {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
@@ -22,7 +22,10 @@ defmodule Discuss.CommentsChannel do
 
     case Repo.insert(changeset) do
       {:ok, comment} ->
-        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+          %{comment: Repo.preload(comment, :user)}
+        )
+
         {:reply, :ok, socket}
       {:error, _reason} ->
         {:reply, {:error, %{errors: changeset}}, socket}
